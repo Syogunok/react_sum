e1d2367c13a6309e8fd8ab1f49508945c12a3610
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-10-18 20:26:01 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <div>
-    <Sum comment="Sum of 2 and 3 is:" a={2} b={3} />
-    <Sum comment="Sum of -5 and 5 is:" a={-5} b={5} />
-    <Sum comment="Sum of 10 and 0 is:" a={10} b={0} />
-    <Sum comment="Sum of 0 and 5 is:" a={0} b={5} />
-    <Sum comment="Sum of 0 and 0 is:" a={0} b={0} />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
   </div>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,6 +1,5 @@
-export const Sum = ({ comment, a = 0, b = 0 }) => (
-  <div>
-    <p>{comment}</p>
-    <p>{a + b}</p>
-  </div>
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of {a} and {b} is {a + b}
+  </p>
 );
